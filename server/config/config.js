24bde4d10ca26ca94320d27e2f2554d05e97368d
rodrigo sanchez peregrino <rodi1828@hotmail.com>
2020-05-08 14:55:13 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 }
 else{
-  urlDB = 'mongodb+srv://rodrixbad:rodriguito123@cluster0-lj7rf.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
